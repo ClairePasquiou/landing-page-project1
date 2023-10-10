cfb626299c43fe1567b55559ce0b335544bc0c40
Claire Pasquiou <clairepasquiou@hotmail.com>
2023-10-10 14:07:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
- <h1>Hello World</h1>
+        <h1>Hello World</h1>
+        <h2>Testing</h2>
       </header>
     </div>
   );
 }
-
