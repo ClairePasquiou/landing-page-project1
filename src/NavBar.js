45a9370519f41818018d47804545b4798edc952f
Claire Pasquiou <clairepasquiou@hotmail.com>
2023-10-10 15:07:38 +0200

    Navigation first elements

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,11 +5,12 @@ import logo1 from "./assets/logo1.png";
 
 export default function NavBar() {
   return (
-    <div className="Navigation">
-      <div>
+    <nav className="navbar">
+      <div className="container">
+      <div className="logo">
         <img className="img-fluid" src={logo1} alt="Logo" />
       </div>
-      <div>
+      <div className="nav-elements">
         <ul>
           <li>
             <a href="/">Home</a>
@@ -27,7 +28,8 @@ export default function NavBar() {
             <a href="/">Contact</a>
           </li>
         </ul>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 }
